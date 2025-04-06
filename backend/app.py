99354cac17ff03234edd06dabf0c49daed57c397
Shortballer2 <shanem0902@gmail.com>
2025-04-06 00:43:03 -0400

    IdeaSubmit

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+import smtplib
+from email.message import EmailMessage
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -14,3 +16,27 @@ def submit():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+EMAIL_ADDRESS = "contact@dawsonandmathis.com"   # Your Gmail address
+EMAIL_PASSWORD = "edvo oaun kzsn bnsg"       # App password from above
+
+def send_email(data):
+    msg = EmailMessage()
+    msg["Subject"] = "New Idea Submission"
+    msg["From"] = EMAIL_ADDRESS
+    msg["To"] = EMAIL_ADDRESS  # You can send to the same account
+
+    msg.set_content(f"""
+New Submission from the campaign site:
+
+Submission Type: {data.get("followup")}
+Name: {data.get("name")}
+Email: {data.get("email")}
+Type: {data.get("submission_type")}
+Category: {data.get("category")}
+Description: {data.get("description")}
+    """)
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        smtp.send_message(msg)
