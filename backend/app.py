2980c8528a359d84602b359f1c167bee28bbcf41
Shortballer2 <shanem0902@gmail.com>
2025-04-06 01:29:52 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 import smtplib
 from email.message import EmailMessage
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 import os
+from flask_cors import CORS
+CORS(app)
+
 
 app = Flask(__name__)
 CORS(app)  # Allow cross-origin requests (from dawsonandmathis.com)
